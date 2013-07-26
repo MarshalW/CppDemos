45eb587f2d373f9a4688fec5c927cb71edb5a074
Marshal Wu <marshal.wu@gmail.com>
2013-07-26 13:23:52 +0800

    最简单的代码，打印helloworld

---
+++ src/CppDemos.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : CppDemos.cpp
+// Author      : marshal
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "中文显示没有问题！" << endl; // prints !!!Hello World!!!
+	return 0;
+}
